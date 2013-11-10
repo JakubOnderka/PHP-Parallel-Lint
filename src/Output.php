f56eb5f1ba443cd009fb5bcf5c38d7160e7e702c
Jakub Onderka <jakub.onderka@gmail.com>
2013-11-10 13:26:27 +0100

    Renamed setWriter method in MultipleWriter to addWriter

--- src/Output.php
+++ src/Output.php
@@ -157,18 +157,27 @@ class MultipleWriter implements IWriter
     /** @var IWriter[] */
     protected $writers;
 
+    /**
+     * @param IWriter[] $writers
+     */
     public function __construct(array $writers)
     {
         foreach ($writers as $writer) {
-            $this->setWriter($writer);
+            $this->addWriter($writer);
         }
     }
 
-    public function setWriter(IWriter $writer)
+    /**
+     * @param IWriter $writer
+     */
+    public function addWriter(IWriter $writer)
     {
         $this->writers[] = $writer;
     }
 
+    /**
+     * @param $string
+     */
     public function write($string)
     {
         foreach ($this->writers as $writer) {
