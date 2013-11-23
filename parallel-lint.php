501b5b92c129a4412eb681343a1c0d5321bcf942
Jakub Onderka <jakub.onderka@gmail.com>
2013-11-23 14:57:00 +0100

    Fixed autoload path

--- parallel-lint.php
+++ parallel-lint.php
@@ -39,7 +39,27 @@ Usage:
     exit;
 }
 
-require_once __DIR__ . '/vendor/autoload.php';
+$files = array(
+  __DIR__ . '/../../autoload.php',
+  __DIR__ . '/vendor/autoload.php'
+);
+
+$autoloadFileFound = false;
+foreach ($files as $file) {
+    if (file_exists($file)) {
+        require $file;
+        $autoloadFileFound = true;
+        break;
+    }
+}
+
+if (!$autoloadFileFound) {
+    die(
+      'You need to set up the project dependencies using the following commands:' . PHP_EOL .
+      'curl -s http://getcomposer.org/installer | php' . PHP_EOL .
+      'php composer.phar install' . PHP_EOL
+    );
+}
 
 try {
     $manager = new PhpParallelLint\Manager;
