e2d16ef3f674a70f2752d393fe78519e219bf0d8
Jakub Onderka <jakub.onderka@gmail.com>
2014-08-19 16:19:52 +0200

    Changed version to 0.7.1 :-)

--- parallel-lint.php
+++ parallel-lint.php
@@ -30,7 +30,7 @@ Options:
  * Help
  */
 if (!isset($_SERVER['argv'][1]) || in_array('-h', $_SERVER['argv']) || in_array('--help', $_SERVER['argv'])) { ?>
-PHP Parallel Lint version 0.7.2
+PHP Parallel Lint version 0.7.1
 -----------------------------
 Usage:
     parallel-lint [sa] [-p php] [-e ext] [-j num] [--exclude dir] [files or directories]
