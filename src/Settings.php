30934383b6c517c906caf4aa4a0072518e81014c
Scott Dutton <scott.dutton@sykescottages.co.uk>
2018-08-14 21:36:31 +0100

    Use current PHP (rebase)

--- src/Settings.php
+++ src/Settings.php
@@ -145,6 +145,11 @@ class Settings
         $arguments = new ArrayIterator(array_slice($arguments, 1));
         $settings = new self;
 
+        //use the currently invoked php as the default if possible
+        if (defined(PHP_BINARY)) {
+            $settings->phpExecutable = PHP_BINARY;
+        }
+
         foreach ($arguments as $argument) {
             if ($argument{0} !== '-') {
                 $settings->paths[] = $argument;
